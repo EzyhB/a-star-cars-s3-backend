2eb75f7030b5d7e3d006dc56fb0476db3f80f6b0
EzyhB <ezyhbmail@gmail.com>
2023-02-11 19:14:23 +0000

    added router for get and post

---
+++ routes/images.ts
@@ -0,0 +1,7 @@
+import express from "express";
+import { getImageByID, postImageToS3 } from "../controllers/images";
+
+const router = express.Router();
+
+router.route("/api/images/:id").get(getImageByID).post(postImageToS3);
+// router.route("/api/images/:id");
