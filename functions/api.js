2bc6062e35d2e39590358c431ffa51790a819e4a
EzyhB <ezyhbmail@gmail.com>
2023-03-08 08:10:39 +0000

    new build script and change to folder structure

--- functions/api.js
+++ functions/api.js
@@ -1,5 +1,5 @@
 import express from "express";
-import imagesRouter from "./routes/images.js";
+import imagesRouter from "../routes/images.js";
 import cors from "cors";
 import serverless from "serverless-http";
 
@@ -7,11 +7,7 @@ const app = express();
 
 app.use(cors());
 
-app.use("/api/v1", imagesRouter);
+app.use("/.netlify/functions/api", imagesRouter);
 // app.use("/.netlify/functions", imagesRouter);
-
-app.listen(3001, () => {
-  console.log("Express server listening...");
-});
 
 module.exports.handler = serverless(app);
