b058405a4d9f7cda8e16b796313f9efe81848c89
EzyhB <ezyhbmail@gmail.com>
2023-03-08 07:36:14 +0000

    added exports handler

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import express from "express";
 import imagesRouter from "./routes/images.js";
 import cors from "cors";
+import serverless from "serverless-http";
 
 const app = express();
 
 app.use(cors());
 
 app.use("/api/v1", imagesRouter);
+// app.use("/.netlify/functions", imagesRouter);
 
 app.listen(3001, () => {
   console.log("Express server listening...");
 });
+
+module.exports.handler = serverless(app);
