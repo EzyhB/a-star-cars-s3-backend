aba9c8d0a419c56b8a15ac95d73e8990a533abde
EzyhB <ezyhbmail@gmail.com>
2023-03-11 16:26:24 +0000

    using multer-s3 to handle the incoming data now

--- routes/images.js
+++ routes/images.js
@@ -1,17 +1,17 @@
 import express from "express";
 import { postImageToS3 } from "../controllers/images.js";
 import multer from "multer";
-import { getImagesFromS3 } from "../s3.js";
+import { getImagesFromS3, postImageToS3Multer } from "../s3.js";
 
-const upload = multer({
-  dest: "uploads/",
-  limits: { fileSize: 50 * 1024 * 1024 },
-});
+// const upload = multer({
+//   dest: "uploads/",
+//   limits: { fileSize: 50 * 1024 * 1024 },
+// });
 
 const ImageRouter = express.Router();
 
 ImageRouter.route("/images/:id")
   .get(getImagesFromS3)
-  .post(upload.array("images[]"), postImageToS3);
+  .post(upload.array("images[]"), postImageToS3Multer());
 
 export default ImageRouter;
--- s3.js
+++ s3.js
@@ -10,7 +10,7 @@ const accessKeyID = process.env.THE_ACCESS_KEY;
 const secretAccessKey = process.env.THE_SECRET_KEY;
 
 // AWS.config.update({
-//   accessKeyId: "your-access-key-id",
+//   accessKeyId: "your-access-key-id" astarcarsales-bucket,
 //   secretAccessKey: "your-secret-access-key",
 // });
 
@@ -20,25 +20,78 @@ const s3 = new AWS.S3({
   secretAccessKey: secretAccessKey,
 });
 
+// Set up multer-s3 middleware
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "astarcarsales-bucket",
+    acl: "public-read",
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, req.params.id + "/" + file.originalname);
+    },
+  }),
+  limits: { fileSize: 50 * 1024 * 1024 },
+});
+
 //Upload file to S3
 const uploadImage = async (files, id) => {
-  const folderName = id.toString();
-  const uploadPromises = files.map((file) => {
-    const fileStream = fs.createReadStream(file.path);
-    const key = `${folderName}/${file.originalname}`; // add the folder name "uploads" and the ID to the S3 key
+  //   const folderName = id.toString();
+  //   const uploadPromises = files.map((file) => {
+  //     const fileStream = fs.createReadStream(file.path);
+  //     const key = `${folderName}/${file.originalname}`; // add the folder name "uploads" and the ID to the S3 key
+  //     const uploadParams = {
+  //       Bucket: bucketName,
+  //       Body: fileStream,
+  //       Key: key,
+  //     };
+  //     return s3.upload(uploadParams).promise();
+  //   });
+  //   const results = await Promise.all(uploadPromises);
+  //   return results;
+};
 
-    const uploadParams = {
-      Bucket: bucketName,
-      Body: fileStream,
-      Key: key,
-    };
+// Route handler for POST /api/images/:id
+const postImageToS3Multer = async (req, res) => {
+  try {
+    // Get the images from the request
+    const ID = req.params.id;
 
-    return s3.upload(uploadParams).promise();
-  });
+    if (!req.headers["content-type"].includes("multipart/form-data")) {
+      console.log("Invalid request format, expected multipart/form-data.");
 
-  const results = await Promise.all(uploadPromises);
+      return res.status(400).send({
+        message: "Invalid request format, expected multipart/form-data.",
+      });
+    }
 
-  return results;
+    // Use multer-s3 middleware to upload the files to S3
+    upload.array("images[]")(req, res, async function (err) {
+      if (err) {
+        console.log("Error uploading images:", err);
+        return res.status(500).send({ message: "Error uploading images." });
+      }
+
+      const uploadedFiles = req.files.map((file) => {
+        return {
+          filename: file.originalname,
+          url: file.location,
+        };
+      });
+
+      res.status(200).json({
+        message: "Uploaded files:",
+        payload: uploadedFiles,
+      });
+    });
+  } catch (error) {
+    console.error("error here", error);
+    res
+      .status(500)
+      .send({ message: "An error occurred while processing the request." });
+  }
 };
 
 //Download file from S3
@@ -70,4 +123,4 @@ const getImagesFromS3 = async (req, res) => {
   }
 };
 
-export { uploadImage, getImagesFromS3 };
+export { uploadImage, getImagesFromS3, postImageToS3Multer };
