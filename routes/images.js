23944a78d7eeb2106e456c9e400084fbea0724d5
EzyhB <ezyhbmail@gmail.com>
2023-04-02 20:50:52 +0100

    uploading without errors now

--- routes/images.js
+++ routes/images.js
@@ -13,6 +13,12 @@ const ImageRouter = express.Router();
 
 ImageRouter.route("/images/:id")
   .get(getImagesFromS3)
-  .post(uploadMulter.array("images[]"), postImageToS3);
+  .post(uploadMulter.array("images[]"), (req, res) => {
+    res.json({
+      response: 200,
+      message: "you did it dumbass",
+    });
+  });
+// .post(uploadMulter.array("images[]"), postImageToS3);
 
 export default ImageRouter;
