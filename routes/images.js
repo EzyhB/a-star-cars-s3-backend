5d4877780e4ed90dcfd4a83c2a0bacc14867440a
EzyhB <ezyhbmail@gmail.com>
2023-04-02 21:28:24 +0100

    removed unused things in routes

--- routes/images.js
+++ routes/images.js
@@ -1,13 +1,6 @@
 import express from "express";
-import { postImageToS3 } from "../controllers/images.js";
-// import { postImageToS3 } from "../controllers/images.js";
-import multer from "multer";
-import { getImagesFromS3, postImageToS3Multer, uploadMulter } from "../s3.js";
 
-const upload = multer({
-  dest: "uploads/",
-  limits: { fileSize: 50 * 1024 * 1024 },
-});
+import { getImagesFromS3, uploadMulter } from "../s3.js";
 
 const ImageRouter = express.Router();
 
@@ -19,6 +12,5 @@ ImageRouter.route("/images/:id")
       message: "you did it dumbass",
     });
   });
-// .post(uploadMulter.array("images[]"), postImageToS3);
 
 export default ImageRouter;
