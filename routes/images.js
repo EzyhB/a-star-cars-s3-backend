e29854642f6d9041159596529087079da78cdb76
EzyhB <ezyhbmail@gmail.com>
2023-04-11 00:06:18 +0100

    before the big mess

--- routes/images.js
+++ routes/images.js
@@ -1,16 +1,15 @@
 import express from "express";
 
-import { getImagesFromS3, uploadMulter } from "../s3.js";
+import { uploadMulter } from "../s3.js";
 
 const ImageRouter = express.Router();
 
 ImageRouter.route("/images/:id")
-  .get(getImagesFromS3)
-  .post(uploadMulter.array("images[]"), (req, res) => {
-    res.json({
-      response: 200,
-      message: "you did it dumbass",
-    });
+.post(uploadMulter.array("images[]"), (req, res) => {
+  res.json({
+    response: 200,
+    message: "you did it dumbass",
   });
+});
 
 export default ImageRouter;
