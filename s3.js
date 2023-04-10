f838b2647aadc2dff4e91e87a5e5d56d3f1d74d0
EzyhB <ezyhbmail@gmail.com>
2023-04-11 00:07:15 +0100

    changed s3.js

--- s3.js
+++ s3.js
@@ -1,7 +1,6 @@
 import AWS from "aws-sdk";
 import dotenv from "dotenv";
 import multer from "multer";
-import fs from "fs";
 import multerS3 from "multer-s3";
 
 dotenv.config();
@@ -42,93 +41,4 @@ const uploadMulter = multer({
   boundary: "----WebKitFormBoundary7MA4YWxkTrZu0gW",
 });
 
-//Upload file to S3
-const uploadImage = async (files, id) => {
-  const folderName = id.toString();
-  const uploadPromises = files.map((file) => {
-    const fileStream = fs.createReadStream(file.location);
-    // const fileStream = new stream.PassThrough();
-    const key = `${folderName}/${file.originalname}`; // add the folder name "uploads" and the ID to the S3 key
-    const uploadParams = {
-      Bucket: bucketName,
-      Body: fileStream,
-      Key: key,
-    };
-
-    return s3.upload(uploadParams).promise();
-  });
-  const results = await Promise.all(uploadPromises);
-  return results;
-};
-
-// Route handler for POST /api/images/:id
-const postImageToS3Multer = async (req, res) => {
-  try {
-    // Get the images from the request
-    const ID = req.params.id;
-
-    if (!req.headers["content-type"].includes("multipart/form-data")) {
-      console.log("Invalid request format, expected multipart/form-data.");
-
-      return res.status(400).send({
-        message: "Invalid request format, expected multipart/form-data.",
-      });
-    }
-
-    // Use multer-s3 middleware to upload the files to S3
-    uploadMulter.array("images[]")(req, res, async function (err) {
-      if (err) {
-        console.log("Error uploading images:", err);
-        return res.status(500).send({ message: "Error uploading images." });
-      }
-
-      const uploadedFiles = req.files.map((file) => {
-        return {
-          filename: file.originalname,
-          url: file.location,
-        };
-      });
-
-      res.status(200).json({
-        message: "Uploaded files:",
-        payload: uploadedFiles,
-      });
-    });
-  } catch (error) {
-    console.error("error here", error);
-    res
-      .status(500)
-      .send({ message: "An error occurred while processing the request." });
-  }
-};
-
-//Download file from S3
-const getImagesFromS3 = async (req, res) => {
-  const ID = req.params.id.toString();
-
-  const listParams = {
-    Bucket: bucketName,
-    Prefix: ID,
-  };
-
-  try {
-    const data = await s3.listObjectsV2(listParams).promise();
-
-    const objects = data.Contents;
-
-    objects.forEach((obj) => {
-      const s3Stream = s3
-        .getObject({ Bucket: listParams.Bucket, Key: obj.Key })
-        .createReadStream();
-      s3Stream.pipe(res);
-    });
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send({
-      message: "An error occurred while processing the request.",
-      error: err,
-    });
-  }
-};
-
-export { uploadImage, getImagesFromS3, uploadMulter, postImageToS3Multer };
+export { uploadMulter };
